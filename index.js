f05bd07e60713f65a90b00d6477c18bb7a360f0b
cdyfng <xiaofeng_chen2007@163.com>
2019-11-07 13:08:20 +0800

    tranfer in limit times

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // import http from 'http';
-import ScatterJS from 'scatterjs-core';
-import ScatterEOS from 'scatterjs-plugin-eosjs';
-import Eos from 'eosjs';
+import ScatterJS from "scatterjs-core";
+import ScatterEOS from "scatterjs-plugin-eosjs";
+import Eos from "eosjs";
 // import config from 'config'
 //import {config} from '../config.js';
-let config = require('../config')
+let config = require("../config");
 
 // Don't forget to tell ScatterJS which plugins you are using.
 // http.createServer((req, res) => {
@@ -14,34 +14,62 @@ let config = require('../config')
 
 // console.log('Server running at http://127.0.0.1:1337/');
 
-
-
-ScatterJS.plugins( new ScatterEOS() );
+ScatterJS.plugins(new ScatterEOS());
 
 const network = ScatterJS.Network.fromJson({
-    blockchain:'eos',
-    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    host:'nodes.get-scatter.com',
-    port:443,
-    protocol:'https'
+  blockchain: "eos",
+  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+  host: "nodes.get-scatter.com",
+  port: 443,
+  protocol: "https"
 });
 
-ScatterJS.connect(config.eos_from, {network}).then(connected => {
-    if(!connected) return console.error('no scatter');
-    console.log('connect ok')
+ScatterJS.connect(
+  config.eos_from,
+  { network }
+).then(connected => {
+  if (!connected) return console.error("no scatter");
+  console.log("connect ok");
 
-    const eos = ScatterJS.eos(network, Eos);
-    console.log('eos from', config.eos_from)
-    console.log('eos to', config.eos_to)
+  const eos = ScatterJS.eos(network, Eos);
+  console.log("eos from", config.eos_from);
+  console.log("eos to", config.eos_to);
 
-    ScatterJS.login().then(id => {
-        if(!id) return console.error('no identity');
-        const account = ScatterJS.account('eos');
-        const options = {authorization:[`${account.name}@${account.authority}`]};
-        eos.transfer(account.name, config.eos_to, '0.0001 EOS', account.name, options).then(res => {
-            console.log('sent: ', res);
-        }).catch(err => {
-            console.error('error: ', err);
-        });
-    });
+  ScatterJS.login().then(id => {
+    if (!id) return console.error("no identity");
+    const account = ScatterJS.account("eos");
+    const options = { authorization: [`${account.name}@${account.authority}`] };
+
+    var interval;
+    var MAXCOUNT = 200;
+    var count = 0;
+    function remind() {
+      if (count < MAXCOUNT) {
+        console.log(
+          "interval5... ",
+          count++,
+          account.name,
+          config.eos_to,
+          options
+        );
+        eos
+          .transfer(account.name, config.eos_to, "0.0001 EOS", count, options)
+          .then(res => {
+            console.log("sent: ", res);
+          })
+          .catch(err => {
+            console.error("error: ", err);
+          });
+      } else {
+        console.log("clearInterval(interval);  interval5... ");
+        clearInterval(interval);
+      }
+    }
+    console.log("setInterval", count, MAXCOUNT);
+    interval = setInterval(function() {
+      remind();
+    }, 10000); //5秒循环调用执行remind()函数
+
+    // //清除延时程序
+  });
 });
